1759354e9560bfbf2f0ef0b5a1cf44208ca8ebfe
dominic-cs50 <dominic888uk@gmail.com>
2025-08-22 09:56:52 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 
 $username = ($_POST["username"]);
 $email = ($_POST["email"]);
-$password = ($_POST["password']);
+$password = password_hash($_POST["password'], PASSWORD_DEFAULT);
 
 $link = new mysqli("shrilvyn_Accounts", "Shrine1234", "Shrine1234!", "DatabaseTable");
 
@@ -14,9 +14,6 @@ if ($link === false) {
 $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
 $stmt->bind_param("ss", $user, $pass);
 
-// Set parameters and execute
-$user = $_POST['username'];
-$pass = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
 $stmt->execute();
 
 echo "New record created successfully";
